364915f0750df9845f26b6ac5e7fff569d2297cf
Ramon Saraiva <ramonsaraiva@gmail.com>
2013-11-06 09:31:07 -0200

    added q & r generator

--- mhknapsack.py
+++ mhknapsack.py
@@ -1,4 +1,5 @@
 import random
+from fractions import gcd
 
 class MHKnapsack(object):
 	w = []
@@ -16,8 +17,21 @@ class MHKnapsack(object):
 			self.w.append(r)
 			self.wsum += r
 
-ks = MHKnapsack()
-ks.generate_superinc(15, 10)
-print ks.w
-print ks.wsum
+	def generate_q_and_r(self):
+		#q > wsum & r ~ [1, q) and coprime to q -> gcd(r, q) == 1
+		self.q = random.randint(self.wsum, self.wsum + 99999)
 
+		self.r = random.randint(1, self.q)
+		while gcd(self.r, self.q) != 1:
+			self.r = random.randint(1, self.q)
+
+ks = MHKnapsack()
+print 'generating w...'
+ks.generate_superinc(15, 10)
+print 'w: '
+print ks.w
+print 'wsum: ' + str(ks.wsum)
+print 'generating q & r...'
+ks.generate_q_and_r()
+print 'q: ' + str(ks.q)
+print 'r: ' + str(ks.r)
